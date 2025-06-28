358036cabaccde01b4e53a7d9aec71f2e618578b
ignacioz-ai <ignacio_zu@outlook.com>
2025-06-28 19:25:58 -0300

    LanguageNotice.vue erased

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
-import LanguageNotice from './components/LanguageNotice.vue';
 </script>
 
 <template>
   <div class="min-h-screen">
     <Header />
-    <LanguageNotice />
     <router-view></router-view>
   </div>
 </template>
