1708cc05da6112233e4df8fc6c0743d7f84050c3
ignacioz-ai <ignacio_zu@outlook.com>
2025-06-28 18:45:54 -0300

    refresh

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import Header from './components/Header.vue';
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n({ useScope: 'global' })
+const i18nKey = ref(0)
+
+watch(locale, () => {
+  i18nKey.value++
+})
 </script>
 
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen" :key="i18nKey">
     <Header />
     <router-view></router-view>
   </div>
