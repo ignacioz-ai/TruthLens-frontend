cdeeeb205ba38acc05ce250d512058dd54df497d
ignacioz-ai <ignacio_zu@outlook.com>
2025-06-28 18:38:06 -0300

    Still debugging

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import App from './App.vue'
 import router from './router'
 import i18n from './plugins/i18n'
 import { loadLocaleMessages } from './i18n/loadLocale'
+import en from '../public/locales/en.json' // Importa el JSON de inglés por defecto
+
+// load default language
+if (i18n.global.getLocaleMessage('en') === undefined || Object.keys(i18n.global.getLocaleMessage('en')).length === 0) {
+  i18n.global.setLocaleMessage('en', en)
+  i18n.global.locale.value = 'en'
+}
 
 async function bootstrap() {
   // Load preferred or default language before mounting the app
